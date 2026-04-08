6ddf3c336296e43e6c9cc15aa8ad7e94caf53e4b
jcarloshg <carlosj12336@gmail.com>
2026-04-08 14:24:49 -0600

    save info

--- src/main/java/com/clean_archi/crud_items/HelloController.java
+++ src/main/java/com/clean_archi/crud_items/HelloController.java
@@ -1,9 +1,11 @@
 package com.clean_archi.crud_items;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/info")
 public class HelloController {
 
     @GetMapping("/helloword")
