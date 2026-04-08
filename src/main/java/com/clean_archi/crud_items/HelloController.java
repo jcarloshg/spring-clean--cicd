c06af52268bfbc263117717967844f2aac97c43e
jcarloshg <carlosj12336@gmail.com>
2026-04-08 14:27:25 -0600

    ad post example

--- src/main/java/com/clean_archi/crud_items/HelloController.java
+++ src/main/java/com/clean_archi/crud_items/HelloController.java
@@ -1,6 +1,8 @@
 package com.clean_archi.crud_items;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,11 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+
+    @PostMapping("/create")
+    public String create(@RequestBody HelloRequest request) {
+        return "Created: " + request.name();
+    }
 }
+
+record HelloRequest(String name) {}
