83997fb2bcf25f96106b358cc8f3f25331e5cf49
jcarloshg <carlosj12336@gmail.com>
2026-04-08 14:43:45 -0600

    create the controller auth

---
+++ src/main/java/com/clean_archi/crud_items/AuthController.java
@@ -0,0 +1,11 @@
+package com.clean_archi.crud_items;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/info")
+public class AuthController {
+
+}
+
