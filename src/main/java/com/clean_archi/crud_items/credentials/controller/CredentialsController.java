0fbe3dcdac22e8e082974bc59f13aebd7bba0016
jcarloshg <carlosj12336@gmail.com>
2026-04-08 17:34:00 -0600

    save all

--- src/main/java/com/clean_archi/crud_items/credentials/controller/CredentialsController.java
+++ src/main/java/com/clean_archi/crud_items/credentials/controller/CredentialsController.java
@@ -1,10 +1,12 @@
 package com.clean_archi.crud_items.credentials.controller;
 
 import com.clean_archi.crud_items.credentials.service.CredentialsService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,5 +38,22 @@ public class CredentialsController {
             @RequestHeader(value = "Accept", defaultValue = "application/json") String accept,
             @RequestHeader(value = "X-Custom-Header", required = false) String customHeader) {
         return ResponseEntity.ok(credentialsService.getAllHeaders(acceptLanguage, accept, customHeader));
+    }
+
+    @GetMapping("/accepted")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String accepted() {
+        return "Request accepted for processing";
+    }
+
+    @GetMapping("/no-content")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void noContent() {
+    }
+
+    @GetMapping("/created")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String created() {
+        return "Resource created successfully";
     }
 }
